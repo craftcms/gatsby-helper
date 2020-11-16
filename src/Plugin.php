cc8696c8614ff9ab1bf5b367046e20b3ebfb6764
andris-sevcenko <asevcenko@gmail.com>
2020-11-16 13:11:44 +0200

    Cleanup.

--- src/Plugin.php
+++ src/Plugin.php
@@ -25,15 +25,6 @@ use craft\services\Gql;
 use yii\base\Event;
 
 /**
- * Craft plugins are very much like little applications in and of themselves. We’ve made
- * it as simple as we can, but the training wheels are off. A little prior knowledge is
- * going to be required to write a plugin.
- *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
- * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
- *
- * https://docs.craftcms.com/v3/extend/
- *
  * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
  * @since 1.0.0
  *
